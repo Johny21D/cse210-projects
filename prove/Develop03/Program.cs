dcd7d0aa235ea2590f4af38f331f3e82b25c79d9
Johny Suy <suy.d.johny15@gmail.com>
2026-05-30 23:51:06 -0600

    add comments

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,3 +1,12 @@
+// Exceeds requirements:
+// It's stores multiple scriptures and picks one at random
+// each time the program runs, so the user gets a different scripture every session.
+
+//Only hides VISIBLE words, words that are hidden will stay hidden
+
+// Input validation only 'continue' or 'quit' are accepted, if user type different 
+//input, prompt them again until they enter right word what the prompt ask.
+
 class Program
 {
     static void Main(string[] args)
